64b51f2d3b892f3d7e03769242331a402b6bd337
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-22 18:03:28 +0700

    parameter

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const express = require('express');
 
 const app = express();
 
+//route - link
+app.get('/', (req, res) => {
+    res.send('<h3>Hello</h3>');
+});
+
+app.get('/about', (req, res) => res.send('Khoa Pham Training'));
+
+app.get('/sayhello/:ten/:tuoi', (req, res) => {
+    const { ten, tuoi } = req.params;
+    res.send(`Xin chao toi la ${ten}, ${tuoi} tuoi`);
+});
+
 app.listen(3000);
+
+
+//npm init -y
+//npm install express
+//node index
