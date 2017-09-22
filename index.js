4479cd57fb9cef9ab46cc7c170de8a3230c29b26
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-22 18:24:54 +0700

    class Tinh

--- index.js
+++ index.js
@@ -14,9 +14,36 @@ app.get('/sayhello/:ten/:tuoi', (req, res) => {
     res.send(`Xin chao toi la ${ten}, ${tuoi} tuoi`);
 });
 
+app.get('/tinh/:soA/:soB/:tenPhepTinh', (req, res) => {
+    //4 + 5 = 9
+});
+
 app.listen(3000);
 
 
 //npm init -y
 //npm install express
 //node index
+
+class Tinh {
+    constructor(soA, soB, tenPhepTinh) {
+        this.soA = soA;
+        this.soB = soB;
+        this.tenPhepTinh = tenPhepTinh;//CONG TRU NHAN CHIA
+    }
+
+    getResultString() {
+        const { tenPhepTinh, soA, soB } = this;
+        let dau = '+';
+        if(tenPhepTinh === 'TRU') dau = '-';
+        if(tenPhepTinh === 'NHAN') dau = '*';
+        if(tenPhepTinh === 'CHIA') dau = '/';
+        const chuoiPhepTinh = `${soA} ${dau} ${soB}`;
+        return `${chuoiPhepTinh} = ${eval(chuoiPhepTinh)}`;
+    }
+}
+
+const pt1 = new Tinh(4, 5, 'NHAN');
+console.log(pt1.getResultString()); //4 + 5 = 9
+
+// eval('4 + 5') // 9
